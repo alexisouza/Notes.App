706345b1c6e6d89f34301ad3ebd083dfc814b6f7
alexisouza <alex.is@outlook.com>
2022-11-06 06:59:12 -0300

    Adicionar arquivos de projeto.

---
+++ Notes/Models/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Models
+{
+    internal class About
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "Esse app foi escrito em XAML e C# com .NET MAUI";
+    }
+}
